a4a009c99a66e3f8a47e8ce836d2290b53e16d8e
jpvm33jp <80127431+jpvm33jp@users.noreply.github.com>
2022-11-16 22:29:16 -0300

    datos agregados
    se agregaron los datos

--- client/src/Accordion.js
+++ client/src/Accordion.js
@@ -2,8 +2,10 @@ import Accordion from 'react-bootstrap/Accordion';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Figure from 'react-bootstrap/Figure';
+import Certi from './Certificados'
+import { Curso,Curso3,Duracion4,Duracion1,Duracion2,Duracion3,Empresa3,Idioma} from './Cursos';
+import {Tipo1,Area} from './Certificados';
 
- 
  
 
 
@@ -22,14 +24,15 @@ function AlwaysOpenExample() {
             src="https://www.uai.cl/assets/uploads/2018/09/logo-uai.png"
         />
         <Figure.Caption>
-            Empresa Certificadora.
+            <Empresa3/>
         </Figure.Caption>
         </Figure></ListGroup.Item>
-      <ListGroup.Item>Título</ListGroup.Item>
-      <ListGroup.Item>Tipo</ListGroup.Item>
-      <ListGroup.Item>Área</ListGroup.Item>
-      <ListGroup.Item>Fecha de Obtención</ListGroup.Item>
-      <ListGroup.Item>Vigencia  </ListGroup.Item>
+      <ListGroup.Item>Título <Certi/></ListGroup.Item>
+      <ListGroup.Item>Tipo <Tipo1/></ListGroup.Item>
+      <ListGroup.Item>Área <Area/></ListGroup.Item>
+      <ListGroup.Item> <p> Fecha de Obtención </p>
+        6/9/2021</ListGroup.Item>
+      <ListGroup.Item>Vigencia <p> 20/4/2022</p> </ListGroup.Item>
     </ListGroup>      </Accordion.Body>
     </Accordion.Item>
     <Accordion.Item eventKey="1">
